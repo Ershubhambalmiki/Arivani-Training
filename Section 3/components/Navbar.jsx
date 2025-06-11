820727641863cb4209ac7ccdec2571358dc3319e
Shubham Balmiki <shubhamkumarn880@gmail.com>
2025-06-11 11:30:02 +0530

    add navbar and add flowbite-react

--- Section 3/components/Navbar.jsx
+++ Section 3/components/Navbar.jsx
@@ -1,77 +1,30 @@
 "use client"
 import { useRouter } from 'next/navigation';
 import React from 'react'
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarToggle } from "flowbite-react";
+import Link from "next/link";
 
-const Navbar = () => {
+const MyNavbar = () => {
   const router = useRouter();
 
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <button href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Next.js Practice
-            </span>
-          </button>
-          <button
-            data-collapse-toggle="navbar-dropdown"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-dropdown"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <button
-                  onClick={() => router.push("/")}
-                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-                  aria-current="page"
-                >
-                  Home
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={() => router.push("/facebook-post")}
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Facebook Post
-                </button>
-              </li>
-              <li>
-                <button
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  SignUp
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <Navbar fluid rounded>
+      <NavbarBrand as={Link} href="/" className='cursor-pointer'>
+        {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Next.js practice</span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+      
+       
+         <button className='cursor-pointer' onClick={()=>router.push("facebook-post")}>facebook-post</button>
+        <button className='cursor-pointer' onClick={()=>router.push("user-signup")}>user-signup</button>
+      </NavbarCollapse>
+    </Navbar>
 
     </>
   )
 }
 
-export default Navbar
+export default MyNavbar
